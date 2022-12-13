98781cc976f3467c62a524d2976e61f475dcb6c4
danlawand <daniel.lawand@usp.br>
2022-12-13 20:41:11 -0300

    client.c com função que check os argumentos de entrada e update do readme

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 * Acredito que esteja certo o algoritmo, porém não adicionei prints de teste ou mesmo o código do cliente, e nem mesmo os testes.
 * Próximo passo é fazer TESTES DE CONECTIVIDADE e não de caminho. Testes que verificam se as árvores estão de acordo são muito custosos, acredito que valha a pena começar apenas com testes de conectividade mesmo.
 
+## Comentário 13/12/2022
+* Compile: gcc -g client.c -o client
+
 ### TO-DO
 * Código do cliente
 * Testes para conectividade
--- src/client.c
+++ src/client.c
@@ -4,6 +4,22 @@
 #include <string.h>
 // #include "forest.h"
 
+// Verifica se os argumentos de entrada estão corretos
+void checkInputArgs(int argc) {
+	if (argc < 2) {
+		printf("- To run this file you need to execute as following:\n");
+		printf("-    ./exe <test_name> <test_number> [flag_for_verbose_output]\n");
+		printf("-    verbose flag is 'v'\n\n");
+		exit(EXIT_FAILURE);
+	}
+	if (argc < 3) {
+		printf("It needs the file's name and the test number.\n");
+		exit(EXIT_FAILURE);
+	}
+    return;
+}
+
+// Lida com as informações do arquivo de entrada
 void handleFile(FILE* fp) {
     char * line = NULL;
     size_t len = 0;
@@ -16,17 +32,31 @@ void handleFile(FILE* fp) {
     }
 
     if (line) free(line);
+    return;
 }
 
 
 int main(int argc, char * argv[])
 {
-    FILE * fp;
-    fp = fopen("test1.txt", "r");
-    if (fp == NULL) exit(EXIT_FAILURE);
+    // Verifica se os argumentos de entrada estão corretos
+    checkInputArgs(argc);
 
-    handleFile(fp);
+    // Verifica se os prints serão quiet ou verbosos
+	char flag_output = 'q';
+	if (argc == 4) flag_output = 'v';
 
-    fclose(fp);
+
+    // Lê o arquivo de entrada e Verifica se o arquivo de entrada existe
+    FILE * arquivo_entrada;
+    arquivo_entrada = fopen("test1.txt", "r");
+	if (arquivo_entrada == NULL) {
+		perror("Error while opening the file of arquivo_entrada.\n");
+		exit(EXIT_FAILURE);
+	}
+
+    // Lida com as informações do arquivo de entrada
+    handleFile(arquivo_entrada);
+
+    fclose(arquivo_entrada);
     exit(EXIT_SUCCESS);
 }
